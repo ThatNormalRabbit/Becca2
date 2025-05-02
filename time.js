bc3f30f66ff05aae3496e0d8035011ecf104c3b2
Rabbit <132155496+ThatNormalRabbit@users.noreply.github.com>
2025-05-02 02:56:48 -0300

    Add files via upload

---
+++ time.js
@@ -0,0 +1,16 @@
+var startDate = new Date("Apr 17, 2025 00:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = now - startDate;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+}, 1000);
+
