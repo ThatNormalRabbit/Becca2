3b8cd42126876ea1f0ecafdde73a9180952b9cda
Rabbit <132155496+ThatNormalRabbit@users.noreply.github.com>
2025-05-12 02:20:02 -0300

    Update time.js

--- time.js
+++ time.js
@@ -10,10 +10,10 @@ var x = setInterval(function() {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-  if (months > 1){
+  if (months => 1){
     document.getElementById("demo").innerHTML = months + "m " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
   } else {
-      document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
   }
 
 }, 1000);
