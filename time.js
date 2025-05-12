45dae3f1baa80418e1b122be56b37afe3c95a6f9
Rabbit <132155496+ThatNormalRabbit@users.noreply.github.com>
2025-05-12 02:14:56 -0300

    Update time.js

--- time.js
+++ time.js
@@ -5,12 +5,15 @@ var x = setInterval(function() {
   var distance = now - startDate;
 
   var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var days = Math.floor((distance %  (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  document.getElementById("demo").innerHTML = months + "m " + days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  
+  if (months > 1){
+    document.getElementById("demo").innerHTML = months + "m " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  } else {
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }
 
-}, 1000;
+}, 1000);
