3eb69324127226fc96f2a1a34d0f016030d605b5
Rabbit <132155496+ThatNormalRabbit@users.noreply.github.com>
2025-05-17 01:14:29 -0300

    Update time.js

--- time.js
+++ time.js
@@ -10,6 +10,6 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("demo").innerHTML = months + "m " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+document.getElementById("demo").innerHTML = months + "mes " + days + "dias " + hours + "horas " + minutes + "minutos " + seconds + "segundos ";
 
 }, 1000);
